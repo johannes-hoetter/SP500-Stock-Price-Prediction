01cebefcdf3ad6c2aad64e30d1a8904a53901e31
johannes-hoetter <johannes.hoetter@smail.inf.h-brs.de>
2018-12-06 17:26:36 +0100

    Prepped for Django Backend

--- app/app.py
+++
@@ -1,7 +0,0 @@
-from machine_learning.sp500 import SP500Predictor
-
-if __name__ == '__main__':
-    sp500_predictor = SP500Predictor()
-    sp500_predictor.activate()
-
-    print(sp500_predictor.predict('AAPL'))
