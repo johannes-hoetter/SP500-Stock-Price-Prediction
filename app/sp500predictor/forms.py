5d5b99a5ea13a08f5c2126d161bfff3228f44a45
johannes-hoetter <johannes.hoetter@smail.inf.h-brs.de>
2018-12-11 16:38:21 +0100

    Commented forms.py

--- app/sp500predictor/forms.py
+++ app/sp500predictor/forms.py
@@ -1,10 +1,20 @@
+"""
+- author: Johannes Hötter (https://github.com/johannes-hoetter)
+- version: 1.0
+- last updated on: 05/12/2018
+
+This file contains several User Input Forms for the SP500 Predictor Application
+"""
+
 from django import forms
 from django.forms import Form, TextInput, CharField
 
+# Input Form for when the User gives Stock Data as a String
 class StockInputForm(forms.Form):
     stock_names = forms.CharField(label='Stock', required=False)
     stock_names.label = ""
 
+# Input Form for when the User wants a number of recommendations
 class StockNumForm(forms.Form):
     num_stocks = forms.CharField(label='Number of Recommendations', required=False,  widget=TextInput(attrs={'type':'number'}))
     num_stocks.label = ""
